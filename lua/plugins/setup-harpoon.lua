c4d7a65dd4984692b085c1f2944c8b02faaa5cb6
menno prinzhorn <mennoprinzhorn@gmail.com>
2024-03-25 15:30:10 +0100

    remove keymaps, interferes with current keymapping

--- lua/plugins/setup-harpoon.lua
+++ lua/plugins/setup-harpoon.lua
@@ -8,10 +8,10 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>h;", function() harpoon:list():select(5) end)
+-- vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(1) end)
+-- vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(2) end)
+-- vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(3) end)
+-- vim.keymap.set("n", "<leader>h;", function() harpoon:list():select(5) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
